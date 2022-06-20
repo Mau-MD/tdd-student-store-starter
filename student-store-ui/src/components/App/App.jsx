38111d00ae7625829c9cf3362e92b03e9fbb8b66
Mauricio Munoz <maumd@fb.com>
2022-06-20 11:53:24 -0700

    Adding state and initial routes

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,10 +1,26 @@
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Sidebar from "../Sidebar/Sidebar"
-import Home from "../Home/Home"
-import "./App.css"
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
+import Home from "../Home/Home";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import NotFound from "../NotFound/NotFound";
+import ProductDetail from "../ProductDetail/ProductDetail";
+
+import { useState } from "react";
 
 export default function App() {
+  // State
+
+  const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // type => {itemId, quantity}
+  const [shoppingCart, setShoppingCart] = useState([]);
+  const [checkoutForm, setCheckoutForm] = useState([]);
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -12,9 +28,13 @@ export default function App() {
           {/* YOUR CODE HERE! */}
           <Navbar />
           <Sidebar />
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products/:productId" element={<ProductDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </main>
       </BrowserRouter>
     </div>
-  )
+  );
 }
