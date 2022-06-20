405fe13967b6cb9c94163de4511cee48e6b4cfc9
Mauricio Munoz <maumd@fb.com>
2022-06-20 13:18:55 -0700

    Adding logic for basic handlers

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -19,17 +19,74 @@ export default function App() {
 
   // type => {itemId, quantity}
   const [shoppingCart, setShoppingCart] = useState([]);
-  const [checkoutForm, setCheckoutForm] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [checkoutForm, setCheckoutForm] = useState({});
 
-  const handleOnToggle = () => {};
+  const handleOnToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-  const handleAddItemToCart = () => {};
+  const handleAddItemToCart = (productId) => {
+    const productExists = shoppingCart.find(
+      (product) => product.itemId === productId
+    );
 
-  const handleRemoveItemFromCart = () => {};
+    // If the product exists, add one to the product quantity
+    if (productExists !== undefined) {
+      // Add one to the desired product
+      const mappedShoppingCart = shoppingCart.map((product) => {
+        if (product.itemId === productId) {
+          return { ...product, quantity: product.quantity + 1 };
+        }
+        return product;
+      });
+      setShoppingCart(mappedShoppingCart);
+      return;
+    }
 
-  const handleOnCheckoutFormChange = () => {};
+    setShoppingCart([...shoppingCart, { itemId: productId, quantity: 1 }]);
+  };
 
-  const handleOnSubmitCheckoutForm = () => {};
+  const handleRemoveItemFromCart = (productId) => {
+    const productExists = shoppingCart.find(
+      (product) => product.itemId === productId
+    );
+
+    // We do nothing if the product doesnt exist
+    if (productExists === undefined) return;
+
+    let shouldRemoveItem = false;
+
+    // Remove one to the desired product
+    const mappedShoppingCart = shoppingCart.map((product) => {
+      if (product.itemId === productId) {
+        if (product.quantity - 1 === 0) shouldRemoveItem = true;
+        return { ...product, quantity: product.quantity - 1 };
+      }
+      return product;
+    });
+
+    // Filter the array to remove the item
+    if (shouldRemoveItem) {
+      const filteredShoppingCart = shoppingCart.filter(
+        (product) => product !== productId
+      );
+      setShoppingCart(filteredShoppingCart);
+      return;
+    }
+    setShoppingCart(mappedShoppingCart);
+  };
+
+  const handleOnCheckoutFormChange = (name, value) => {
+    setCheckoutForm({
+      ...checkoutForm,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmitCheckoutForm = () => {
+    //TODO
+  };
 
   return (
     <div className="app">
