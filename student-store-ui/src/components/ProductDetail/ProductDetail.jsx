86f025a1d53bce9b78958f2b1dad88c53e0bb11a
Mauricio Munoz <maumd@fb.com>
2022-06-20 13:40:52 -0700

    Adding Product Detail Component

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-const ProductDetail = () => {
-  return <div>ProductDetail</div>;
+const ProductDetail = ({ handleAddItemToCart, handleRemoveItemToCart }) => {
+  const { productId } = useParams();
+  return <div className="product-detail">{productId}</div>;
 };
 
 export default ProductDetail;
