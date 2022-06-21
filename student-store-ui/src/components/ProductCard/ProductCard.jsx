c4d71e376c311959110b063810d26b309b921ec8
Mauricio Munoz <maumd@fb.com>
2022-06-21 14:43:57 -0700

    Styling Product Card Component

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,34 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductCard = ({
   product,
-  productId,
-  quantity,
+  productId = 0,
+  quantity = 0,
   handleAddItemToCart,
   handleRemoveItemToCart,
   showDescription,
 }) => {
   return (
     <div className="product-card">
+      <div className="product-img-container">
+        <Link to={`/products/${productId}`}>
+          <img src={product.image} alt={product.name} />
+        </Link>
+      </div>
       <div className="product-name">{product.name}</div>
       <div className="product-price">${product.price}</div>
       {showDescription && (
         <div className="product-description">{product.description}</div>
       )}
-      <Link to={`/products/${productId}`}>
-        <img src={product.image} alt={product.name} />
-      </Link>
-      <button className="add" onClick={() => handleAddItemToCart(productId)}>
-        Add
-      </button>
-      <button
-        className="remove"
-        onClick={() => handleRemoveItemToCart(productId)}
-      >
-        Add
-      </button>
-      <div className="product-quantity">{quantity}</div>
+      <div className="product-buttons-container">
+        <button className="add" onClick={() => handleAddItemToCart(productId)}>
+          Add
+        </button>
+        <div className="product-quantity">{quantity}</div>
+        <button
+          className="remove"
+          onClick={() => handleRemoveItemToCart(productId)}
+        >
+          Remove
+        </button>
+      </div>
     </div>
   );
 };
