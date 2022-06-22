feac99705641aa6626b0c2933ae4f8882c9fe92c
Mauricio Munoz <maumd@fb.com>
2022-06-22 09:53:23 -0700

    Adding Footer Styles

--- student-store-ui/src/components/Footer/Footer.jsx
+++ student-store-ui/src/components/Footer/Footer.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container">
+      <div className="footer-category">
+        <div className="footer-category-header">Categories</div>
+        <div className="footer-category-text">All Categories</div>
+        <div className="footer-category-text">Clothing</div>
+        <div className="footer-category-text">Food</div>
+      </div>
+      <div className="footer-category">
+        <div className="footer-category-header">Categories</div>
+        <div className="footer-category-text">All Categories</div>
+        <div className="footer-category-text">Clothing</div>
+        <div className="footer-category-text">Food</div>
+      </div>
+      <div className="footer-category">
+        <div className="footer-category-header">Categories</div>
+        <div className="footer-category-text">All Categories</div>
+        <div className="footer-category-text">Clothing</div>
+        <div className="footer-category-text">Food</div>
+      </div>
+      <div className="footer-category">
+        <div className="footer-category-header">Categories</div>
+        <div className="footer-category-text">All Categories</div>
+        <div className="footer-category-text">Clothing</div>
+        <div className="footer-category-text">Food</div>
+      </div>
+      <div className="footer-category">
+        <div className="footer-category-header">Categories</div>
+        <div className="footer-category-text">All Categories</div>
+        <div className="footer-category-text">Clothing</div>
+        <div className="footer-category-text">Food</div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
