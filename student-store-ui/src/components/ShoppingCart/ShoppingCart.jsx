32aa179c272ba62731ec925aa20a8ff27cb59001
Mauricio Munoz <maumd@fb.com>
2022-06-22 11:31:50 -0700

    Adding shopping cart table

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -15,24 +15,36 @@ const ShoppingCart = ({ isOpen, products, shoppingCart }) => {
 
   return (
     <div className="shopping-cart">
-      {shoppingCartProducts && shoppingCartProducts.length > 0 ? (
-        shoppingCartProducts.map((shoppingCartProduct) => (
-          <div>
-            <div className="card-product-name">{shoppingCartProduct.name}</div>
-            <div className="card-product-quantity">
-              {shoppingCartProduct.quantity}
-            </div>
-            <div className="card-product-quantity">
-              ${shoppingCartProduct.price}
-            </div>
+      <p className="shopping-cart-products-heading">Shopping Cart Products</p>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Individual Price</th>
+          <th>Total</th>
+        </tr>
+        {shoppingCartProducts && shoppingCartProducts.length > 0 ? (
+          shoppingCartProducts.map((shoppingCartProduct) => (
+            <tr className="card-product-container">
+              <td className="card-product-name">{shoppingCartProduct.name}</td>
+              <td className="card-product-quantity">
+                {shoppingCartProduct.quantity}
+              </td>
+              <td className="card-product-price">
+                ${shoppingCartProduct.price}
+              </td>
+              <td className="card-product-price">
+                ${shoppingCartProduct.price * shoppingCartProduct.quantity}
+              </td>
+            </tr>
+          ))
+        ) : (
+          <div className="notification">
+            <AiOutlineShoppingCart size={40} />
+            <p>No items added to cart yet. Start shopping now!</p>
           </div>
-        ))
-      ) : (
-        <div className="notification">
-          <AiOutlineShoppingCart size={40} />
-          <p>No items added to cart yet. Start shopping now!</p>
-        </div>
-      )}
+        )}
+      </table>
     </div>
   );
 };
